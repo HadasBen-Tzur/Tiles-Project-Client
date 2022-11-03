d7c173a9e5bb2863ef40f1dada80cd4b2d1d8fe1
Hadas <Hadas.BenTzur@agamimp.net>
2022-11-03 08:26:46 +0200

    feath(fix-loclost)

--- src/services/axios.util.ts
+++ src/services/axios.util.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+export const environment = process.env.NODE_ENV;
+
+export const isDevelopment = environment === "development";
+
+const baseURL = isDevelopment
+  ? "http://localhost:8080/"
+  : (import.meta.env.API_LOCATION as string | undefined) ?? "";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL,
 });
